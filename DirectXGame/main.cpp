7c7c04bf86cc22d366da2a144486038c6822e606
cho-yohan <k023g1037@g.neec.ac.jp>
2024-09-01 20:45:01 +0900

    AL3_GAME

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -98,7 +98,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2C_07_チョ_ヨハン_AL3");
+	win->CreateGameWindow(L"Only_Jump");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
